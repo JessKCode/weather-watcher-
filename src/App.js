def3acdb21862f84cba9d56e11c2c3cf331c01de
JessKCode <82847253+JessKCode@users.noreply.github.com>
2021-07-12 16:47:41 -0700

    added a link to my GitHub repository

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App">
       <h1>Weather Watcher 🔎 </h1>
+      <p>
+        Coded by Jessica K Carter {""}
+      <a href="https://github.com/JessKCode/weather-watcher-"
+      target="_blank"
+      >
+        Open-sourced on GitHub
+      </a>
+      </p>
     </div>
   );
 }
