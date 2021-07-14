b5e150a5114ab48e21969d3d3531a6ab028ec7f9
JessKCode <82847253+JessKCode@users.noreply.github.com>
2021-07-14 13:45:33 -0700

    Edit basic HTML

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 export default function Weather(props){
-  return (
-  <h1>Weather Watcher 🔎 </h1>
+ return (
+  <div className="Weather">
+      <form className="search">
+        <input
+          type="shearch"
+          placeholder="Search for a City"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <p className="day">Day, Date</p>
+      <p className="time">Time</p>
+      <h1 className="city">City</h1>
+      <p className="description"> Weather description </p>
+      <div className="icon">Icon</div>
+      <h2 className="temp">36°</h2>
+      <p className="unit">C° | F°</p>
+      <p className="humidity">Humidity</p>
+      <p className="wind">Wind</p>
+    </div>
     );
 }
